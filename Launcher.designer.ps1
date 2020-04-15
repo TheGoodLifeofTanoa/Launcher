dc5181be027ed68ad2eba880b36dfc895cc72957
MinersWIn <minerswin@minerswin.de>
2020-04-15 16:11:47 +0200

    Added first PowerShell Files

---
+++ Launcher.designer.ps1
@@ -0,0 +1,12 @@
+$Form1 = New-Object -TypeName System.Windows.Forms.Form
+function InitializeComponent
+{
+$Form1.SuspendLayout()
+#
+#Form1
+#
+$Form1.Text = [System.String]'Form1'
+$Form1.ResumeLayout($false)
+Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
+}
+. InitializeComponent
